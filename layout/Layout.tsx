9ced0d1fc355f3784e45e171cd619b89800c62ff
d.kozlov <d.kozlov@hottelecom.by>
2021-10-12 11:20:20 +0300

    add HOC withLayout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -4,8 +4,9 @@ import { Footer } from './Footer/Footer';
 import { Sidebar } from './Sidebar/Sidebar';
 import cn from 'classnames';
 import styles from './Layout.module.css';
+import { FunctionComponent } from 'react';
 
-export const Layout = ({ children }: LayoutProps): JSX.Element => {
+const Layout = ({ children }: LayoutProps): JSX.Element => {
 	return (
 		<>
 			<Header />
@@ -16,4 +17,14 @@ export const Layout = ({ children }: LayoutProps): JSX.Element => {
 			<Footer />
 		</>
 	);
+};
+
+export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
+	return function withLayoutComponent(props: T): JSX.Element {
+		return (
+			<Layout>
+				<Component {...props} />
+			</Layout>
+		);
+	};
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 import { Button, Htag, P, Rating, Tag } from '../components';
-import { Layout } from '../layout/Layout';
+import { withLayout } from '../layout/Layout';
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
 	const [rating, setRating] = useState<number>(4);
 
 	return (
-		<Layout>
+		<>
 			<Htag tag="h1">Text</Htag>
 			<P>asdasdav adfa sda as da d sd asd addasdasdasdasd sdasdas</P>
 			<P size="s">asdasdav adfa sda as da d sd asd addasdasdasdasd sdasdas</P>
@@ -27,6 +27,8 @@ export default function Home(): JSX.Element {
 			</Tag>
 			<Tag color="red">QWE</Tag>
 			<Rating isEditable rating={rating} setRating={setRating} />
-		</Layout>
+		</>
 	);
 }
+
+export default withLayout(Home);
